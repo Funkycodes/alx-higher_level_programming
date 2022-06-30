911dedb172805864d0b4db6eeea0fa8dd9db12bf
Funkycodes <otakus3nnin@gmail.com>
2022-06-30 23:39:37 +0100

    Ridddim

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    from sys import argv
+    args = argv[1:]
+    argv_length = len(args)
+    if argv_length == 0:
+        print("{:d} arguments.".format(argv_length))
+    elif argv_length == 1:
+        print("{:d} argument:".format(argv_length))
+    else:
+        print("{:d} arguments:".format(argv_length))
+
+    argv_length = 1
+    for arg in args:
+        print("{:d}: {:s}".format(argv_length, arg))
+        argv_length += 1
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    args_sum = 0
+    for arg in argv[1:]:
+        args_sum += int(arg)
+    print("{:d}".format(args_sum))
