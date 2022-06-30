e695d075baf7ada4656006546f849c7958fab5ea
otakus3nnin <otakus3nnin@gmail.com>
2022-06-29 19:14:29 -0700

    Fumkky

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ print("Last digit of {:d} is {:d} and is".format(number, last_digit), end=' ')
 if last_digit > 5:
     print("greater than 5")
 elif last_digit == 0:
-    print("zero")
+    print("0")
 else:
     print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def remove_char_at(str, n):
     strcopy = ''
     for i in range(len(str)):
