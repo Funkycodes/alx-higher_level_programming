7455529fc6c7c346f9705134262c365fcba83f97
Funkycodes <otakus3nnin@gmail.com>
2022-08-02 17:54:45 +0100

    eiddim

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry
+Contains Class(es):
+    BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """Base Class for geometrical objects"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
