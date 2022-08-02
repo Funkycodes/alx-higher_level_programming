dacf9d17b79b310cda95d6656a8644bc2c8455f3
Funkycodes <otakus3nnin@gmail.com>
2022-08-02 09:22:23 +0100

    riddim

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 1-my_list
+Contains Mylist class that inherits from and extends list class
+"""
+
+
+class MyList(list):
+    """Extends list class"""
+
+    def print_sorted(self):
+        """print sorted instance"""
+        return sorted(self)
