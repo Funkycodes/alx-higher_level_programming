a768ad0507352e830f8eff469eb5c8f6f8266ddf
Funkycodes <otakus3nnin@gmail.com>
2022-08-03 02:46:46 +0100

    r

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3
+"""Module 7-add_item
+
+Script that save list rep of command line arguments to json file
 """
-Module 7-add_item
-
-Contains function that adds and saves to Python obj to JSON file; loads objects
-
-# run with ./7-add_item.py
-#
-# cat add_item.json ; echo ""
-# expect output: []
-#
-# ./7-add_item.py some random args
-# cat add_item.json ; echo ""
-# expect output: ["some", "random", "args"]
-
-"""
-
-
+import json
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-
-try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
-
-save_to_json_file(existing_content + argv[1:], filename)
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+with open("add_item.json", "w") as f:
+    json.dump(argv[1:], f)
