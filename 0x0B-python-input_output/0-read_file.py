18b2fce58ad85df1dda5b9cdf9388f0265af1eec
Funkycodes <otakus3nnin@gmail.com>
2022-08-03 01:59:29 +0100

    a

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Module 0-read_file
+
 Contains function that reads and prints file contents
 """
 
 
 def read_file(filename=""):
     """Print file contents"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read().strip('\n'))
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
