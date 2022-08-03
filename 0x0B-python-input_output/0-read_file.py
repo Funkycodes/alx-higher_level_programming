329c3c0c2daae95fb356a6c03f467b8b6334604d
Funkycodes <otakus3nnin@gmail.com>
2022-08-03 01:57:08 +0100

    rid

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Module 0-read_file
+Contains function that reads and prints file contents
+"""
+
+
 def read_file(filename=""):
+    """Print file contents"""
     with open(filename, encoding="utf-8") as f:
         print(f.read().strip('\n'))
-
-
-read_file(filename="README.md")
