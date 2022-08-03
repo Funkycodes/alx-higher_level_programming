0db6495f18c6012da1300827f91ec7a0b450c273
Funkycodes <otakus3nnin@gmail.com>
2022-08-03 02:05:24 +0100

    riddim

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Module 1-write_file
+
+Contains function that writes to file
+"""
+
+
 def write_file(filename="", text=""):
-    with open(filename, mode='w', encoding='utf-8') as f:
-        return (f.write(text))
+    """Write text to filename"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return(f.write(text))
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Module 2-append_write
+
+Contains function that appends to file
+"""
+
+
 def append_write(filename="", text=""):
+    """Append text to existing file"""
     with open(filename, 'a', encoding="utf-8") as f:
         return (f.write(text))
