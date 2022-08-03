79b99bc0105dc2a7a5fa5c764afc55924bf70dda
Funkycodes <otakus3nnin@gmail.com>
2022-08-03 01:54:58 +0100

    rid

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,5 +3,5 @@ import json
 
 
 def load_from_json_file(filename):
-    with open(filename, mode="r", encoding="UTF-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         return (json.load(f))
