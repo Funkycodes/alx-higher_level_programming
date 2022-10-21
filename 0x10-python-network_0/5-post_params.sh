ed1318c1f054fa16952ffece536ba418311d971e
unknown <otakus3nnin@gmail.com>
2022-10-21 03:04:08 +0100

    riddim

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, displays the body of the response
-curl "$1" -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+# Take in URL, POST key:vals; Usage: ./5-post_params.sh 0.0.0.0:5000/route_6 ; echo ""
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
--- 0x10-python-network_0/README.md
+++ 0x10-python-network_0/README.md
@@ -1,1 +1,1 @@
-readme file for python network directory
+0x10-python-network_0
